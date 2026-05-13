50c0a912c9349f27425edc2a8b251187d3af2b66
Henrique Da Silva <henriquehenry533@gmail.com>
2026-05-13 23:07:52 +0100

    coletas

--- back/app.py
+++ back/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routers import usuario
 from routers import auth
+from routers import coletas
 
 app = FastAPI()
 
 app.include_router(usuario.router)
 app.include_router(auth.router)
+app.include_router(coletas.router)
---
+++ back/routers/coletas.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from database import get_database
+from schemas import ColetaCreate, ColetaOut
+from models import Coletas, Usuarios, Materiais
+
+router = APIRouter(prefix="/coletas", tags=["coletas"])
+
+@router.post("/", response_model=ColetaOut)
+def criar_coleta(coleta: ColetaCreate, db: Session = Depends(get_database)):
+    usuario = db.query(Usuarios).filter(Usuarios.id == coleta.idUsuario).first()
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    
+    material = db.query(Materiais).filter(Materiais.id == coleta.idMaterial).first()
+    if not material:
+        raise HTTPException(status_code=404, detail="Material não encontrado")
+    
+    pontos_ganhos = int(float(coleta.quantidadeKilo) * float(material.pontos))
+    
+    usuario.pontuacao += int(pontos_ganhos)
+    usuario.total_reciclado = float(usuario.total_reciclado) + float(coleta.quantidadeKilo)
+    
+    nova_coleta = Coletas(
+        idUsuario=coleta.idUsuario,
+        idMaterial=coleta.idMaterial,
+        quantidadeKilo=coleta.quantidadeKilo,
+        pontos_ganhos=pontos_ganhos
+    )
+    
+    db.add(nova_coleta)
+    db.commit()
+    db.refresh(nova_coleta)
+    
+    return nova_coleta
+
+@router.get("/{user_id}", response_model=list[ColetaOut])
+def listar_coletas_usuario(user_id: int, db: Session = Depends(get_database)):
+    coletas = db.query(Coletas).filter(Coletas.idUsuario == user_id).all()
+    
+    return coletas
