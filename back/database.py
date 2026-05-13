95fc30a3eeadd55d2daff3743e3d3e5adebd100c
Henrique Da Silva <henriquehenry533@gmail.com>
2026-05-13 01:52:00 +0100

    database

--- back/database.py
+++ back/database.py
@@ -1,1 +1,23 @@
-...
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_database():
+    db = SessionLocal()
+    try:
+        yield db     #yield é um comando que fornece o db para o uso e depois do uso permite fechar a sessao no banco
+    finally:
+        db.close()
