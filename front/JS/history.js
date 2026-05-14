6984381ed83e50501df60e3d141fb96d38adf659
Henrique Da Silva <henriquehenry533@gmail.com>
2026-05-14 21:00:08 +0100

    history.js

---
+++ front/JS/history.js
@@ -0,0 +1,31 @@
+async function Historico_coletas() {
+    user_id = JSON.parse(localStorage.getItem("usuario")).id
+
+    const coletas = await fetch(`http://127.0.0.1:8000/coletas/${user_id}`)
+    if (!coletas.ok) throw new Error("Erro ao listar as coletas do usuario");
+
+    const list_coletas = await coletas.json();
+
+    for (const coleta of list_coletas) {
+
+        material = await fetch(`http://127.0.0.1:8000/materiais/${coleta.idMaterial}`);
+
+        if (!material.ok) {
+            console.error("Erro ao carregar material:", material.status);
+            return;
+        }
+
+        const material_info = await material.json();
+
+        const data = coleta.dataCadastro.split("T")
+
+        const tr = document.createElement("tr")
+        tr.innerHTML = `<td>${data[0]}</td>
+                        <td>${material_info.nome}</td>
+                        <td>${coleta.quantidadeKilo} Kg</td>
+                        <td class="pontos">+${coleta.pontos_ganhos} pts</td>`
+        document.getElementById("table_body").appendChild(tr);
+    }
+}
+
+Historico_coletas();
