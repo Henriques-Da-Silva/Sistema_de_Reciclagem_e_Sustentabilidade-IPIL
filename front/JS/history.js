94e391a72de7e0119cd1b10e6bcb984a0d1e29ea
Henrique Da Silva <henriquehenry533@gmail.com>
2026-05-16 02:31:27 +0100

    botao exportar historico

--- front/JS/history.js
+++ front/JS/history.js
@@ -9,6 +9,11 @@ async function Historico_coletas() {
         if (!coletas.ok) throw new Error("Erro ao listar as coletas do usuario");
     
         const list_coletas = await coletas.json();
+
+        if (!Array.isArray(list_coletas) || list_coletas.length === 0) {
+            document.getElementById("table_body").innerHTML = "<tr><td colspan='4'>Nenhuma coleta encontrada.</td></tr>";
+            return;
+        }
     
         for (const coleta of list_coletas) {
     
@@ -35,5 +40,35 @@ async function Historico_coletas() {
         alert("Erro ao carregar histórico. Tenta novamente.");
     }
 }
+
+btn = document.getElementById("export_historic")
+
+btn.addEventListener("click", () => {
+    const linhas = document.querySelectorAll("table tr");
+
+    let csv = [];
+
+    linhas.forEach(linha => {
+        let cols = linha.querySelectorAll("th, td");
+        let dados = [];
+
+        cols.forEach(col => {
+            let texto = col.innerText.replace(/,/g, "");
+            dados.push(texto);
+        });
+
+        csv.push(dados.join(","));
+    });
+
+    const csvString = csv.join("\n");
+
+    const blob = new Blob([csvString], { type: "text/csv" });
+
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "historico_coletas.csv";
+
+    link.click();
+});
 
 Historico_coletas();
